56cf3e5aed04d84f80ed1638d1c6626c09a52c14
Lilit <lilitmanukyan09@gmail.com>
2021-04-10 13:21:01 +0400

    <commit message>

---
+++ Parkinson_project/tmp/Outliers_detection.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.append(r'..\configs')
+
+import config
+import pandas as pd
+
+df = pd.read_csv(f"../src/data/{config.data_name}.txt")
+df = df.drop(columns=['name'])
+
+features_with_outliers = ["MDVP:Fhi(Hz)", "MDVP:Jitter(%)", "MDVP:Jitter(Abs)", "MDVP:RAP", "MDVP:PPQ", "Jitter:DDP",
+                          "MDVP:Shimmer", "MDVP:Shimmer(dB)", "Shimmer:APQ3", "Shimmer:APQ5", "MDVP:APQ", "Shimmer:DDA",
+                          "NHR", "HNR", "D2", "spread1", "spread2", "PPE"]
+
+outlier_indexes = []
+for col in features_with_outliers:
+    Q1 =  df[col].quantile(0.25)
+    Q3 = df[col].quantile(0.75)
+    IQR = Q3 - Q1
+    outlier_indexes.append(df[(df[col] < (Q1-1.5*IQR)) | (df[col] > (Q3+1.5*IQR))].index.tolist())
+
+flat_list = [item for sublist in outlier_indexes for item in sublist]
+
+outlier_indexes = list(set(flat_list))
+print(len(outlier_indexes))
+
+outliers = df[df.index.isin(outlier_indexes)]
+print(outliers)
+
+#We decided do not delete outliers because data is only 195 (38 outliers).
